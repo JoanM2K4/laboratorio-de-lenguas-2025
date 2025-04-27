c93cf6348f76588bb6011ae87723934c04f5fdbe
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2025-04-27 14:14:57 -0500

    Pagina de Contacto Completa, los siguientes cambios seran dirigidos a la documentacion del codigo

--- contacto.php
+++ contacto.php
@@ -5,96 +5,45 @@
 	<meta charset="utf-8">
 	<title>Contacto</title>
 	<link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="./CSS/contacto.css">
+	<link rel="stylesheet" href="./CSS/CSSPaginas/Style_header.css">
+	<link rel="stylesheet" type="text/css" href="./CSS/CSSPaginas/contacto.css">
 	<link rel="stylesheet" type="text/css" href="./CSS/fuentes.css">
 </head>
 
 <body>
+
+	<div id="ContenedorEncabezado" class="container-fluid"><!--Inicio contendor encabezado-->
+
+		<?php include("Header.html") ?><!--Encabezado-->
+
+	</div><!--Fin contenedor encabezado-->
+
+	<div id="ContenedorMenu" class="container-fluid"><!--Inicio contendor encabezado-->
+
+		<?php include("./Componentes/PaginaInicio/NavInicio.html") ?><!--menu-->
+
+	</div><!--Fin contenedor encabezado-->
+
 	<div id="contendor" class="container"><!--Inicia contendor-->
 
-		<!-- TODO: agregar header -->
-
-		<div class="row">
-			<nav aria-label="breadcrumb">
-				<ol class="breadcrumb">
-					<li class="breadcrumb-item"><a href="">Inicio</a></li>
-					<!--TODO: agregar link a la pagina de inicio-->
-					<li class="breadcrumb-item active"><a href="contacto.php">Contacto</a></li>
-				</ol>
-			</nav>
-
-		</div>
-
-		<div class="row">
-			<div id="frase-grande" class="jumbotron">
-				<div class="container">
-					<h1>Contáctanos</h1>
-					<p>Estamos para ayudar.</p>
-				</div>
-			</div>
-
-		</div>
-
-		<div class="row"><!--Inicio sección-->
-		<div class="col-sm-4 col-md-4">
-				<div class="thumbnail">
-					<div class="contenedor_icono">
-						<img src="./images/contacto/profile.png">
-					</div>
-
-					<div class="caption contenedor_texto font-poppins">
-						<h3>Coordinador</h3>
-						<p>Licenciada Sheila Bultrón</p>
-						<p>Centro Especializado en Lenguas de Coclé</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-4 col-md-4">
-				<div class="thumbnail">
-					<div class="contenedor_icono">
-						<img src="./images/contacto/mobile.png">
-					</div>
-
-					<div class="caption contenedor_texto">
-					<h3>Llamenos</h3>
-						<a href="tel:9060664">Tel.: 906-0664 / Ext. 1225</a>
-						<br>
-						<a href="tel:+50768845248">Cel.: 6884-5248</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-4 col-md-4">
-				<div class="thumbnail">
-					<div class="contenedor_icono">
-						<img src="./images/contacto/email.png">
-					</div>
-
-					<div class="caption contenedor_texto">
-						<h3>Escribenos</h3>
-						<a href="mailto:cel.cocle@utp.ac.pa">cel.cocle@utp.ac.pa</a>
-						<br>
-						<a href="mailto:sheila.bultron@utp.ac.pa">sheila.bultron@utp.ac.pa</a>		
-					</div>
-				</div>
-			</div>
-			
-
-
-
-		</div><!--Final sección-->
+		<?php include("Componentes\contacto\breadcrumb.html") ?> <!-- migas de pan, muestran donde estas-->
+		<?php include("Componentes\contacto\saludo.html") ?> <!-- jumbotron -->
+		<?php include("Componentes\contacto\informacion.html") ?> <!-- cards con informaccion de ontacto -->
+		<?php include("Componentes\contacto\horario.html") ?> <!-- horarios de clases -->
+		<?php include("Componentes\contacto\mapa.html") ?> <!-- ubicacion del centro de lenguas en un mapa -->
 
 	</div><!--termina contendor-->
-
-
 
 	<!--query-->
 	<script src="./js/jquery.js"></script>
 	<!-- Bootstrap JavaScript -->
 	<script src="./js/bootstrap.min.js"></script>
 
-	<footer class="pies">
-
-	</footer>
+	<div id="footer" class="row">
+		<?php
+		include 'Footer.html';  // Incluye el contenido de Footer.html
+		?>
+	</div>
 
 
 </body>
