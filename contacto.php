ffc22345e2f557fe38f4e47e13d190ab0d6c89a3
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2025-04-27 10:51:12 -0500

    creacion de tarjetas en pag Contatcto, mejora en el import de fuentes tipograficcas

---
+++ contacto.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<title>Contacto</title>
+	<link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./CSS/contacto.css">
+	<link rel="stylesheet" type="text/css" href="./CSS/fuentes.css">
+</head>
+
+<body>
+	<div id="contendor" class="container"><!--Inicia contendor-->
+
+		<!-- TODO: agregar header -->
+
+		<div class="row">
+			<nav aria-label="breadcrumb">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="">Inicio</a></li>
+					<!--TODO: agregar link a la pagina de inicio-->
+					<li class="breadcrumb-item active"><a href="contacto.php">Contacto</a></li>
+				</ol>
+			</nav>
+
+		</div>
+
+		<div class="row">
+			<div id="frase-grande" class="jumbotron">
+				<div class="container">
+					<h1>Contáctanos</h1>
+					<p>Estamos para ayudar.</p>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="row"><!--Inicio sección-->
+		<div class="col-sm-4 col-md-4">
+				<div class="thumbnail">
+					<div class="contenedor_icono">
+						<img src="./images/contacto/profile.png">
+					</div>
+
+					<div class="caption contenedor_texto font-poppins">
+						<h3>Coordinador</h3>
+						<p>Licenciada Sheila Bultrón</p>
+						<p>Centro Especializado en Lenguas de Coclé</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-4">
+				<div class="thumbnail">
+					<div class="contenedor_icono">
+						<img src="./images/contacto/mobile.png">
+					</div>
+
+					<div class="caption contenedor_texto">
+					<h3>Llamenos</h3>
+						<a href="tel:9060664">Tel.: 906-0664 / Ext. 1225</a>
+						<br>
+						<a href="tel:+50768845248">Cel.: 6884-5248</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-4">
+				<div class="thumbnail">
+					<div class="contenedor_icono">
+						<img src="./images/contacto/email.png">
+					</div>
+
+					<div class="caption contenedor_texto">
+						<h3>Escribenos</h3>
+						<a href="mailto:cel.cocle@utp.ac.pa">cel.cocle@utp.ac.pa</a>
+						<br>
+						<a href="mailto:sheila.bultron@utp.ac.pa">sheila.bultron@utp.ac.pa</a>		
+					</div>
+				</div>
+			</div>
+			
+
+
+
+		</div><!--Final sección-->
+
+	</div><!--termina contendor-->
+
+
+
+	<!--query-->
+	<script src="./js/jquery.js"></script>
+	<!-- Bootstrap JavaScript -->
+	<script src="./js/bootstrap.min.js"></script>
+
+	<footer class="pies">
+
+	</footer>
+
+
+</body>
+
+</html>
