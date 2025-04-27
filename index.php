1f2aa86f0f51c5b0be341f72e4b7d8d0aed1e3af
ShadowFoxDelta <rexguerra45@hotmail.com>
2025-04-26 23:36:30 -0500

    Pequeño avance del encabezado
    Signed-off-by: Angelo Guerrero <rexguerra45@hotmail.com>

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/CSSPaginas/Style_header.css">
+	<title>Centro de lenguas</title>
+</head>
+<body>
+	<div id="ContenedorEncabezado" class="container"><!--Inicio contendor encabezado-->
+		<div class="panel panel-primary" style="border:none;"><!--Inicio panel encabezado-->
+
+			<?php include("Header.html") ?><!--Encabezado-->
+			<?php include("./Componentes/PaginaInicio/NavInicio.html") ?><!--menu-->
+
+		</div><!--Fin panel encabezado-->
+
+		<div class="panel panel-primary"><!--Inicio panel menu-->
+			
+			
+
+		</div><!--fin panel menu-->
+
+	</div><!--Fin contenedor encabezado-->
+	
+
+</body>
+</html>
