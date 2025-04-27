d7c5a0bdd1d78673eedc1e792e6e1ba30b034718
ShadowFoxDelta <rexguerra45@hotmail.com>
2025-04-27 12:08:05 -0500

    Encabezado completo junto a la barra de navegación
    Signed-off-by: Angelo Guerrero <rexguerra45@hotmail.com>

--- index.php
+++ index.php
@@ -4,24 +4,23 @@
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./CSS/CSSPaginas/Style_header.css">
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/CSSPaginas/Style_header.css">
 	<title>Centro de lenguas</title>
 </head>
 <body>
-	<div id="ContenedorEncabezado" class="container"><!--Inicio contendor encabezado-->
-		<div class="panel panel-primary" style="border:none;"><!--Inicio panel encabezado-->
 
+	
+	
+	<div id="ContenedorEncabezado" class="container-fluid"><!--Inicio contendor encabezado-->
+			
 			<?php include("Header.html") ?><!--Encabezado-->
+
+	</div><!--Fin contenedor encabezado-->
+
+	<div id="ContenedorMenu" class="container-fluid"><!--Inicio contendor encabezado-->
+			
 			<?php include("./Componentes/PaginaInicio/NavInicio.html") ?><!--menu-->
-
-		</div><!--Fin panel encabezado-->
-
-		<div class="panel panel-primary"><!--Inicio panel menu-->
-			
-			
-
-		</div><!--fin panel menu-->
 
 	</div><!--Fin contenedor encabezado-->
 	
