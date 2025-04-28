611e614a0a5057cf4a5e5a7920fc5855bddc709f
ShadowFoxDelta <rexguerra45@hotmail.com>
2025-04-28 03:30:19 -0500

    agregando comentarios

--- PaginaAdmin.php
+++ PaginaAdmin.php
@@ -1,44 +1,52 @@
 <!DOCTYPE html>
-<html>
+<html lang="es"> <!-- Idioma configurado en español -->
 <head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Administrador</title>
-	<link rel="stylesheet" href="./CSS/bootstrap.min.css">
-    <link rel="stylesheet" href="./CSS/CSSPaginas/Style_header.css">
-    <link rel="stylesheet" href="./CSS/CSSPaginas/PaginaAdmin.css">
-    <link rel="stylesheet" href="./CSS/CSSPaginas/Style_footer.css">
-    <link rel="stylesheet" href="./CSS/fuentes.css">
+    <meta charset="UTF-8"> <!-- Configuración de codificación de caracteres -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Para diseño responsive -->
+    
+    <title>Administrador</title> <!-- Título de la página -->
+
+    <!-- Inclusión de archivos CSS -->
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css"> <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./CSS/CSSPaginas/Style_header.css"> <!-- Estilos personalizados del encabezado -->
+    <link rel="stylesheet" href="./CSS/CSSPaginas/PaginaAdmin.css"> <!-- Estilos específicos para la página de administrador -->
+    <link rel="stylesheet" href="./CSS/CSSPaginas/Style_footer.css"> <!-- Estilos personalizados del pie de página -->
+    <link rel="stylesheet" href="./CSS/fuentes.css"> <!-- Estilos de fuentes -->
 </head>
+
 <body>
-	<div id="ContenedorEncabezado" class="container-fluid"><!--Inicio contendor encabezado-->
-			
-			<?php include("Componentes/PaginaAdmin/HeaderAdmin.html") ?><!--Encabezado-->
 
-	</div><!--Fin contenedor encabezado-->
+    <!-- Inicio del contenedor del encabezado -->
+    <div id="ContenedorEncabezado" class="container-fluid">
+        <!-- Inclusión del encabezado desde archivo externo -->
+        <?php include("Componentes/PaginaAdmin/HeaderAdmin.html"); ?>
+    </div>
+    <!-- Fin del contenedor del encabezado -->
 
-	<div id="ContenedorMenu" class="container-fluid"><!--Inicio contendor encabezado-->
-			
-			<?php include("./Componentes/PaginaAdmin/NavAdmin.html") ?><!--menu-->
+    <!-- Inicio del contenedor del menú de navegación -->
+    <div id="ContenedorMenu" class="container-fluid">
+        <!-- Inclusión del menú desde archivo externo -->
+        <?php include("./Componentes/PaginaAdmin/NavAdmin.html"); ?>
+    </div>
+    <!-- Fin del contenedor del menú de navegación -->
 
-	</div><!--Fin contenedor encabezado-->
+    <!-- Inicio del contenedor del contenido principal -->
+    <div class="container contenido">
+        <div class="row"> <!-- Inicio de la fila del contenido -->
 
-	
+            <!-- Inicio del panel de bienvenida -->
+            <div class="col-xs-12">
+                <!-- Inclusión del jumbotron de bienvenida desde archivo externo -->
+                <?php include("./Componentes/PaginaAdmin/JumbotronAdmin.html"); ?>
+            </div>
+            <!-- Fin del panel de bienvenida -->
 
-	<div class="container contenido"><!-- Inicio contenido principal -->
-    <div class="row"><!-- Inicio fila contenido -->
+        </div> <!-- Fin de la fila del contenido -->
+    </div>
+    <!-- Fin del contenedor del contenido principal -->
 
-      <!-- Panel de bienvenida -->
-      <div class="col-xs-12">
-      	
-      	<?php include("./Componentes/PaginaAdmin/JumbotronAdmin.html") ?>
-        
-      </div>
-
-    </div><!-- Fin fila contenido -->
-  </div><!-- Fin contenido principal -->
-
-  <?php include("Footer.html") ?>
+    <!-- Inclusión del pie de página desde archivo externo -->
+    <?php include("Footer.html"); ?>
 
 </body>
 </html>
